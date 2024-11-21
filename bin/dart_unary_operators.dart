f71828a93e7507b80e096ff2d8f799e62363deff
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-11-21 15:24:13 +0600

    dart unary operator practiced

---
+++ bin/dart_unary_operators.dart
@@ -0,0 +1,34 @@
+void main(){
+  var x = 10;
+
+  //unary increment postfix
+
+  // print(x++);
+  // print(x++);
+  // print(x++);
+  // print(x++);
+  // print(x++);
+
+  //unary increment prefix
+
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+
+//   unary decrement postfix
+
+  // print(x--);
+  // print(x--);
+  // print(x--);
+  // print(x--);
+
+//   unary decrement prefix
+
+  print(--x);
+  print(--x);
+  print(--x);
+  print(--x);
+  print(--x);
+}
