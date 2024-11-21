45fc916bf4a5ec06ccd48301dc07b68577f2d88b
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-11-21 15:17:22 +0600

    dart arithmetic operator practices

---
+++ bin/dart_arithmetic_operator.dart
@@ -0,0 +1,14 @@
+void main(){
+  int x = 10;
+  int y = 3;
+
+  int addition = x+y;
+  int subtraction = x-y;
+  int multiplication = x*y;
+  double division = x/y;
+
+  print(addition);
+  print(subtraction);
+  print(multiplication);
+  print(division);
+}
