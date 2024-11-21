8865e6272505b5441919054c402897fe16c880e6
asuselessbrain <arfan18@cse.pstu.ac.bd>
2024-11-21 15:11:34 +0600

    dart data type practiced

---
+++ bin/dart_data_type.dart
@@ -0,0 +1,22 @@
+void main(){
+  int x = 10; //integer type data
+  double y = 20.44; //double type data
+  String name = 'Arfan Ahmed'; //string type data
+  bool isAdmin = true; //boolean type data
+  var friends = ['Ahmed', 'Ambia', 'Tota']; //list data type
+  const courses = ['CIT-311', 'CIT-312', 'CCE-311']; //fixed length list
+  var personalInfo = {
+    'name': 'Arfan',
+    'age': 22,
+    'gender': 'Male',
+    'isStudent': true
+  };                      //map type data
+
+  print(x);
+  print(y);
+  print(name);
+  print(isAdmin);
+  print(friends);
+  print(courses);
+  print(personalInfo);
+}
